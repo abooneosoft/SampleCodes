7a7203adc94859886954ae7c80b69d8fbbec3776
abooneosoft <aboosofttech@gmail.com>
2023-11-20 18:17:21 +0530

    Changes made

---
+++ SampleJava1.java
@@ -0,0 +1,13 @@
+package codes;
+public class SampleJava1 {
+static void m1(){
+    System.out.println("method 1");
+}
+static void m2(){
+    System.out.println("method 2");
+}
+public static void main(String[] args) {
+    m1();
+    m2();
+}
+}
